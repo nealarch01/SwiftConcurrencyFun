57d56190393abc770b410fb30138eb2a57f04d60
Neal Archival <73256760+nealarch01@users.noreply.github.com>
2024-09-17 18:42:46 -0700

    Initial Commit

---
+++ ConcurrencyFun/ConcurrencyFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyFunApp.swift
+//  ConcurrencyFun
+//
+//  Created by Neal Archival on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
